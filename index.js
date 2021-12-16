272c7facd87778de2acfd83ccb9fd218a2814870
TimLee9 <leesh68481@gmail.com>
2021-12-16 07:04:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function func(numbers) {
+  let newArr = [];
+  for (let i = 0; i< numbers.length; i++) {
+    if (numbers[i] > 3) {
+      newArr.push(numbers[i]);    
+    }
+  }
+  return newArr;
+}
+
+const numbers = [1,2,3,4,5,6,7];
+console.log(func(numbers));
