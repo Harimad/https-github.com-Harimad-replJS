df263c710665ae8cfcec50f991533693e3a305f9
Tim Lee <leesh68481@gmail.com>
2021-12-19 14:03:26 +0000

    test

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-console.log();
-console.log();
-console.log();
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
+for (let i = 1; i < 90; i++) {
+    console.log(`<img src="https://harimad.github.io/inflearn_js_img/js_al_img/js_img%20(${i}).png">`);
+}
