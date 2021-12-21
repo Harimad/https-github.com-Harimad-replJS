9fe83f202eff9065a59cb75d84e3b36eab63ad7e
Tim Lee <leesh68481@gmail.com>
2021-12-21 10:06:37 +0000

    A를 #으로

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-function solution(arr){
-  let answer = arr;
-  let sum = [...arr].reduce((prev, curr) => prev + curr) - 100;
-
-  for (let i = 0; i < answer.length-1; i++) {
-    for (let j = i+1; j < answer.length; j++) {
-      if (answer[i] + answer[j] === sum) {
-          answer.splice(j, 1);
-          answer.splice(i, 1);
-      }
-    }
+function solution(s){
+  let answer= '';
+  for (let x of s) {
+    if (x !== 'A') answer += x;
+    else answer += '#';
   }
   return answer;
 }
 
-let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
-console.log(solution(arr));
+let str="BANANA";
+console.log(solution(str));
