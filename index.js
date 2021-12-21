341899f24f7794bc7bb144bd02d666ae91ee429f
Tim Lee <leesh68481@gmail.com>
2021-12-21 09:23:52 +0000

    sec1, 8일골난쟁이

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-for (let i = 1; i < 90; i++) {
-    console.log(`<img src="https://harimad.github.io/inflearn_js_img/js_al_img/js_img%20(${i}).png">`);
+function solution(arr){
+  let answer = arr;
+  let sum = [...arr].reduce((prev, curr) => prev + curr) - 100;
+
+  for (let i = 0; i < answer.length-1; i++) {
+    for (let j = i+1; j < answer.length; j++) {
+      if (answer[i] + answer[j] === sum) {
+          answer.splice(j, 1);
+          answer.splice(i, 1);
+      }
+    }
+  }
+  return answer;
 }
+
+let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
+console.log(solution(arr));
