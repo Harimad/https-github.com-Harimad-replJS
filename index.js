7f22dca9acfa7dff8436d95b503da75d0558b41b
Tim Lee <leesh68481@gmail.com>
2021-12-22 09:38:33 +0000

    중복단어제거

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function solution(s){  
-  let answer="";
-  let arr = new Array(26).fill(false);
+function solution(s){
+  let answer = [];
 
   for (let x of s) {
-    let num = x.charCodeAt() - 96;
-    if (arr[num] === false) {
-      answer += x;
-      arr[num] = true;
-    } 
-  } 
+    if (answer.indexOf(x) === -1) {
+      answer.push(x);
+    }
+  }
+
   return answer;
 }
-console.log(solution("ksekkset"));
+
+let str=["good", "time", "good", "time", "student"];
+console.log(solution(str));
