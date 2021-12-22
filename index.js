fa420d571cd460c615913c191b6ff470f833e9c4
Tim Lee <leesh68481@gmail.com>
2021-12-22 10:01:13 +0000

    문제 이미지 제거

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// function solution(s){  
+//   let answer="";
+//   let arr = new Array(26).fill(false);
+
+//   for (let x of s) {
+//     let num = x.charCodeAt() - 96;
+//     if (arr[num] === false) {
+//       answer += x;
+//       arr[num] = true;
+//     } 
+//   } 
+//   return answer;
+// }
+// console.log(solution("ksekkset"));
+
 function solution(s){
   let answer = [];
 
