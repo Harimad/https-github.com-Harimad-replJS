6c635da698278311d398c70b0ce862ce0a1533d8
Tim Lee <leesh68481@gmail.com>
2021-12-22 13:48:49 +0000

    보이는 학생

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-function solution(arr){         
-  let answer=[arr[0]];
-  for(let i = 1; i < arr.length; i++){
-    if(arr[i] > arr[i-1]) answer.push(arr[i]);
+function solution(arr){
+  let answer = [arr[0]], max = arr[0];
+  for (let i = 0; i < arr.length-1; i++) {
+    if (max < arr[i+1]) {
+      max = arr[i+1];
+      answer.push(arr[i+1]);
+    }
   }
-  return answer;
+  
+  return answer.length;
 }
 
-let arr=[7, 3, 9, 5, 6, 12];
+let arr=[130, 135, 148, 140, 145, 150, 150, 153];
 console.log(solution(arr));
