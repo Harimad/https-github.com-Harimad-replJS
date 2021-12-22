6ad8c0f02eb944d62a78ed07acf754756b805626
Tim Lee <leesh68481@gmail.com>
2021-12-22 14:47:21 +0000

    가위 바위 보

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-function solution(arr){
-  let answer = [arr[0]], max = arr[0];
-  for (let i = 0; i < arr.length-1; i++) {
-    if (max < arr[i+1]) {
-      max = arr[i+1];
-      answer.push(arr[i+1]);
-    }
+function solution(a, b){         
+  let answer=[];
+  for (let i=0; i<a.length; i++) {
+    let diff = a[i]-b[i];
+    if (diff == 1 || diff == -2) answer.push('A');
+    else if (diff == -1 || diff == 2) answer.push('B');
+    else answer.push('D');
   }
-  
-  return answer.length;
+  return answer;
 }
 
-let arr=[130, 135, 148, 140, 145, 150, 150, 153];
-console.log(solution(arr));
+
+let a=[2, 3, 3, 1, 3];
+let b=[1, 1, 2, 2, 3];
+console.log(solution(a, b));
