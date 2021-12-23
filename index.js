a627d41530c412647c1bf5dfcc3ff58a8982a8c1
Tim Lee <leesh68481@gmail.com>
2021-12-23 10:41:48 +0000

    섹션3/ 2. 유효한 팰린드롬

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 function solution(s){
-  let answer='YES', copy = s.toUpperCase();
-
-  for (let i = 0; i < parseInt(s.length/2); i++) {
-    if (copy[i] != copy[s.length-1-i]) answer = 'NO';
+  let answer="YES", copy = s.split(/\W\s/).map((val) => val.toUpperCase());
+  for (let i = 0; i < parseInt(copy.length/2); i++) {
+    if (copy[i].split('').reverse().join('') !== copy[copy.length-1-i]) answer = "NO";
   }
-  console.log(s);
   return answer;
 }
 
-let str="goooG";
+let str="found7, time: study; Yduts; emit, 7Dnuof";
 console.log(solution(str));
