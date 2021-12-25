706327dbf272b48b28d6eb99d87478494a3fb10d
Tim Lee <leesh68481@gmail.com>
2021-12-25 09:11:53 +0000

    섹션3/ 3.숫자만 출력

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-function solution(s){
-  let answer="YES", copy = s.split(/\W\s/).map((val) => val.toUpperCase());
-  for (let i = 0; i < parseInt(copy.length/2); i++) {
-    if (copy[i].split('').reverse().join('') !== copy[copy.length-1-i]) answer = "NO";
-  }
-  return answer;
+function solution(str){
+  let answer=str.match(/\d/g).join('');
+  return parseInt(answer);
 }
 
-let str="found7, time: study; Yduts; emit, 7Dnuof";
+let str="g0en2T0s8eSoft";
 console.log(solution(str));
