c507942c8a1dfe54fbf87c2e25f5a7f2a37e24fb
Tim Lee <leesh68481@gmail.com>
2021-12-25 09:54:04 +0000

    섹션3/ 4.가장 짧은 문자거리

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-function solution(str){
-  let answer=str.match(/\d/g).join('');
-  return parseInt(answer);
+function solution(s, t){
+  let answer=[], count=0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === t) {
+      count = 0;
+      answer.push(count);
+    } else {
+      count++;
+      answer.push(count);
+    }
+  }
+  for (let i = s.length-1; i >= 0; i--) {
+    if (s[i] === t) {
+      count = 0;
+    } else {
+      count++;
+      answer[i] = Math.min(answer[i], count);
+    }
+  }
+
+  return answer;
 }
 
-let str="g0en2T0s8eSoft";
-console.log(solution(str));
+let str="teachermode";
+console.log(solution(str, 'e'));
