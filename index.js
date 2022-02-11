a330b286d7bab530004949eb1c967fead1dfb97f
Tim Lee <leesh68481@gmail.com>
2022-02-11 09:00:35 +0000

    4-4 졸업선물

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-function solution(test){
-  let answer=0, 확인용= [];
-  let studentNum = test[0].length;
-  let testNum = test.length;
-  //모든 학생을 반복문 돌아줌(부르트포스)
-  for (let i = 1; i <= studentNum; i++) { //i가 멘토
-    for (let j = 1; j <= studentNum; j++) { //j가 멘티
-      //멘토인 i가 멘티인 j보다 등수가 높은지 확인해야함
-      let count = 0;
-      for (let k = 0; k < testNum; k++) { //시험 횟수만큼 반복
-        let rankI = 0;
-        let rankJ = 0;
-        for (let l = 0; l < studentNum; l++) { //등수만큼 반복
-          if (test[k][l] === i) rankI = l;
-          if (test[k][l] === j) rankJ = l;
-        }
-        if (rankI < rankJ) count++;
-      }
-      if (count === testNum) {
-        확인용.push([i, j]);
-        answer++;
+function solution(m, product){
+  let answer=0;
+  let student = product.length; //5 명
+  product.sort((a, b) => (a[0]+a[1]) - (b[0]+b[1])); //총비용 오름차순 재정렬
+  
+  for (let i = 0; i < student; i++) { //i번째 상품을 할인받는다
+    let money = m- (product[i][0]/2 + product[i][1]); //할인 받은 상품은 산걸로 가정함, money는 남은 예산을 말한다
+    let count = 1; //할인받은건 샀으니 1부터 시작
+    for (let j = 0; j < student; j++) {
+      if (j !== i && (product[j][0]+product[j][1]) > money) break;
+        //i는 할인된거 이미 샀으니 조건 추가
+      if (j !== i && (product[j][0]+product[j][1] <= money)) { 
+        money -= product[j][0]+product[j][1];
+        count++;
       }
     }
+    answer = Math.max(answer, count);
   }
-  console.log(확인용); //[[3, 1] [3, 2] [4, 2]]
+
   return answer;
 }
 
-let arr=[[3, 4, 1, 2], [4, 3, 2, 1], [3, 1, 4, 2]];
-console.log(solution(arr));
-  
+let arr=[[6, 6], [2, 2], [4, 3], [4, 5], [10, 3]];
+console.log(solution(28, arr));
