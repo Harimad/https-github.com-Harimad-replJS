1b1a99330eb14b838d17a2134c67ad9b63dc1e4d
Tim Lee <leesh68481@gmail.com>
2022-02-11 09:13:26 +0000

    4-4 졸업선물2

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-function solution(m, product){
-  let answer=0;
-  let student = product.length; //5 명
-  product.sort((a, b) => (a[0]+a[1]) - (b[0]+b[1])); //총비용 오름차순 재정렬
-  
-  for (let i = 0; i < student; i++) { //i번째 상품을 할인받는다
-    let money = m- (product[i][0]/2 + product[i][1]); //할인 받은 상품은 산걸로 가정함, money는 남은 예산을 말한다
-    let count = 1; //할인받은건 샀으니 1부터 시작
-    for (let j = 0; j < student; j++) {
-      if (j !== i && (product[j][0]+product[j][1]) > money) break;
-        //i는 할인된거 이미 샀으니 조건 추가
-      if (j !== i && (product[j][0]+product[j][1] <= money)) { 
-        money -= product[j][0]+product[j][1];
-        count++;
-      }
-    }
-    answer = Math.max(answer, count);
-  }
+function solution(m, product) {
+    let result = [];
+    for (let i = 0; i < product.length; i++) {
+        let money = m; //예산
+        let count = 0; //카운트 수
+        product[i][0] = product[i][0] * 0.5 //상품가격 50% 할인
 
-  return answer;
+        //상품가격 + 배송비 의 합으로 새로운 배열을 만들고
+        //오름차순으로 정렬
+        let sortedArr = product
+            .map((value) => value.reduce((acc, cur) => acc + cur))
+            .sort((a, b) => a - b);
+        console.log(sortedArr);
+        //예산으로 살 수 있는 개수 체크
+        for (let j = 0; j < sortedArr.length; j++) {
+            if (sortedArr[j] > money) {
+                break;
+            }
+            money -= sortedArr[j];
+            count++;
+        }
+        result.push(count);
+
+        //할인했던 상품가격 다시 원래 가격으로 바꿔주기
+        product[i][0] = product[i][0] * 2;
+    }
+    return Math.max(...result);
 }
 
-let arr=[[6, 6], [2, 2], [4, 3], [4, 5], [10, 3]];
+let arr = [
+  [6, 6],
+  [2, 2],
+  [4, 3],
+  [4, 5],
+  [10, 3],
+];
 console.log(solution(28, arr));
