cbfe7a05fd44ded110e2803609e6f3683e1b4fe0
Tim Lee <leesh68481@gmail.com>
2022-02-15 08:50:37 +0000

    4-5 K번째 큰 수

--- index.js
+++ index.js
@@ -1,37 +1,18 @@
-function solution(m, product) {
-    let result = [];
-    for (let i = 0; i < product.length; i++) {
-        let money = m; //예산
-        let count = 0; //카운트 수
-        product[i][0] = product[i][0] * 0.5 //상품가격 50% 할인
-
-        //상품가격 + 배송비 의 합으로 새로운 배열을 만들고
-        //오름차순으로 정렬
-        let sortedArr = product
-            .map((value) => value.reduce((acc, cur) => acc + cur))
-            .sort((a, b) => a - b);
-        console.log(sortedArr);
-        //예산으로 살 수 있는 개수 체크
-        for (let j = 0; j < sortedArr.length; j++) {
-            if (sortedArr[j] > money) {
-                break;
-            }
-            money -= sortedArr[j];
-            count++;
-        }
-        result.push(count);
-
-        //할인했던 상품가격 다시 원래 가격으로 바꿔주기
-        product[i][0] = product[i][0] * 2;
+function solution(n, k, card){
+  let answer;
+  let tmp = new Set();
+  for (let i = 0; i < n; i++) {
+    for (let j = i+1; j < n; j++) {
+      for (let l = j+1; l < n; l++) {
+        tmp.add(card[i]+card[j]+card[l]);
+      }
     }
-    return Math.max(...result);
+  }
+  let tmp2 = Array.from(tmp).sort((a, b) => b - a);
+  console.log(tmp2);
+  answer = tmp2[k-1];
+  return answer;
 }
 
-let arr = [
-  [6, 6],
-  [2, 2],
-  [4, 3],
-  [4, 5],
-  [10, 3],
-];
-console.log(solution(28, arr));
+let arr=[13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+console.log(solution(10, 3, arr));
