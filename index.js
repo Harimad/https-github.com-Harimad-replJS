4453529efaa9be4dfb9b4424862ebb42b4f9dd1c
Tim Lee <leesh68481@gmail.com>
2022-02-16 09:52:44 +0000

    5-1 두 배열 합치기(투포인터 알고리즘)

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-function solution(n, k, card){
-  let answer;
-  let tmp = new Set();
-  for (let i = 0; i < n; i++) {
-    for (let j = i+1; j < n; j++) {
-      for (let l = j+1; l < n; l++) {
-        tmp.add(card[i]+card[j]+card[l]);
-      }
-    }
+function solution(arr1, arr2){
+  let answer = [];
+  let n = arr1.length;
+  let m = arr2.length;
+  let p1 = p2 = 0; //두 포인터 값 초기화
+
+  while(p1 < n && p2 < m) {
+    if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
+    else answer.push(arr2[p2++]);
   }
-  let tmp2 = Array.from(tmp).sort((a, b) => b - a);
-  console.log(tmp2);
-  answer = tmp2[k-1];
+  while(p1 < n) answer.push(arr1[p1++]);
+  while(p2 < m) answer.push(arr2[p2++]);
+
   return answer;
 }
 
-let arr=[13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
-console.log(solution(10, 3, arr));
+let a=[1, 3, 5];
+let b=[2, 3, 6, 7, 9];
+console.log(solution(a, b));
