569bb4e95c60d32de0d805c33f4b5ddab2e71b0f
Tim Lee <leesh68481@gmail.com>
2022-02-17 08:08:16 +0000

    5-2 공통원소구하기(Two Pointers Algorithm)

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 function solution(arr1, arr2){
-  let answer = [];
-  let n = arr1.length;
-  let m = arr2.length;
-  let p1 = p2 = 0; //두 포인터 값 초기화
-
-  while(p1 < n && p2 < m) {
-    if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
-    else answer.push(arr2[p2++]);
+  let answer=[];
+  let p1 = p2 = 0;
+  arr1.sort((a,b) => a-b);
+  arr2.sort((a,b) => a-b);
+  
+  while(p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] === arr2[p2]) {
+      answer.push(arr1[p1]);
+      p1++;
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) {
+      p1++;
+    } else if (arr1[p1] > arr2[p2]) {
+      p2++;
+    }
   }
-  while(p1 < n) answer.push(arr1[p1++]);
-  while(p2 < m) answer.push(arr2[p2++]);
-
+  
   return answer;
 }
 
-let a=[1, 3, 5];
-let b=[2, 3, 6, 7, 9];
+let a=[1, 3, 9, 5, 2];
+let b=[3, 2, 5, 7, 8];
 console.log(solution(a, b));
