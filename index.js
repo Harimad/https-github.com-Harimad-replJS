aa6359657c1b05e83aa83b11b5ec24f914438cfd
Tim Lee <leesh68481@gmail.com>
2022-02-20 07:10:40 +0000

    5-4 연속 부분수열 1(투포인터)

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-function solution(m, arr){
-  let answer=0, lt = sum = 0;
+function solution(m, arr) {  
+  let answer = lt = sum = 0;
 
-  for(let rt = 0; rt < arr.length; rt++) {
+  for (let rt = 0; rt < arr.length; rt++) {
     sum += arr[rt];
     while (sum > m) sum -= arr[lt++];
-    // if (sum <= m) //이거 굳이 안해도 위에 while(sum >m)값의 반대 조건이니까 주석 조건과 같은은 조건을 충족한다.
-    answer += rt - lt + 1;
+    if (sum === m) answer++;
   }
   return answer;
 }
-
-console.log(solution(5, [1,3,1,2,3])); // 10
-console.log(solution(100, [1,1,1,1,1])); // 15
+console.log(solution(6, [1, 1, 1, 2, 4])); // 1
+console.log(solution(6, [1, 2, 1, 3, 1, 1, 1, 2])); // 3
+console.log(solution(2, [1, 1, 1])); // 2
