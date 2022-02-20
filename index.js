46c0c5a0b7551b1fda102159c98926989666abe4
Tim Lee <leesh68481@gmail.com>
2022-02-20 06:25:18 +0000

    5-4 연속부분수열2(Two Pointers Algorithm)

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-function solution(arr1, arr2){
-  let answer=[];
-  let p1 = p2 = 0;
-  arr1.sort((a,b) => a-b);
-  arr2.sort((a,b) => a-b);
-  
-  while(p1 < arr1.length && p2 < arr2.length) {
-    if (arr1[p1] === arr2[p2]) {
-      answer.push(arr1[p1]);
-      p1++;
-      p2++;
-    } else if (arr1[p1] < arr2[p2]) {
-      p1++;
-    } else if (arr1[p1] > arr2[p2]) {
-      p2++;
-    }
+function solution(m, arr){
+  let answer=0, lt = sum = 0;
+
+  for(let rt = 0; rt < arr.length; rt++) {
+    sum += arr[rt];
+    while (sum > m) sum -= arr[lt++];
+    // if (sum <= m) //이거 굳이 안해도 위에 while(sum >m)값의 반대 조건이니까 주석 조건과 같은은 조건을 충족한다.
+    answer += rt - lt + 1;
   }
-  
   return answer;
 }
 
-let a=[1, 3, 9, 5, 2];
-let b=[3, 2, 5, 7, 8];
-console.log(solution(a, b));
+console.log(solution(5, [1,3,1,2,3])); // 10
+console.log(solution(100, [1,1,1,1,1])); // 15
